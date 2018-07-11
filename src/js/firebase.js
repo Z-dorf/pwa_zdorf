cd209fb2278350478e95745d80026cfb23735da7
Z-dorf <sebastian.zanon2@gmail.com>
2018-07-11 13:32:04 +0200

    update & fixed

---
+++ src/js/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBaVSYfUzocs683_6ZtISb3pWXitNriEz0",
+    authDomain: "zdorf-s-pwa.firebaseapp.com",
+    databaseURL: "https://zdorf-s-pwa.firebaseio.com",
+    projectId: "zdorf-s-pwa",
+    storageBucket: "zdorf-s-pwa.appspot.com",
+    messagingSenderId: "737492035114"
+  };
+  firebase.initializeApp(config);
---
+++ src/js/push.js
@@ -0,0 +1,13 @@
+const messaging = firebase.messaging();
+
+      messaging.requestPermission()
+      .then(function() {
+        console.log('Notification permission granted.');
+        return messaging.getToken();
+      })
+      .then(function(token) {
+        console.log(token);
+      })
+      .catch(function(err) {
+        console.log('Unable to get permission to notify.', err);
+      })
